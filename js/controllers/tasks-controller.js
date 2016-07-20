ba113a17b1ec3db68a4e99236a61031b695c4931
renanslopes8 <renanslopes8@gmail.com>
2016-07-20 15:24:27 -0300

    Adjust on Id auto increment

--- js/controllers/tasks-controller.js
+++ js/controllers/tasks-controller.js
@@ -1,36 +1,42 @@
 /* global taskApp */
 
   taskApp.factory('TaskFactory', function(){
+    var id =1;
     var tasks = [
-        {id : '1',description : 'Single Page App with AngularJS',active:'false'},
-        {id : '2',description : 'Learn more',active:'false'},
-        {id : '3',description : 'Just one more task here',active:'false'}
+        {id : id++,description : 'Single Page App with AngularJS',active:'false'},
+        {id : id++,description : 'Learn more',active:'false'},
+        {id : id++,description : 'Just one more task here',active:'false'}
     ];
-
     var factory = {};
-
+    
     factory.getTasks = function(){
       return tasks;
     };
-
+    
+    factory.getId = function(){
+      return id;
+    };
+    
+    factory.incId = function(){
+      id ++;
+    };
+  
     return factory;
   });
 
   taskApp.controller('TaskController', function($scope, TaskFactory,$routeParams){
     $scope.tasks = [];
-    $scope.id = 4;
     init();
-
     function init(){
       $scope.tasks = TaskFactory.getTasks();
     }
-
+    
     $scope.addTask = function(){
       $scope.tasks.push({
-        id : $scope.id,
+        id : TaskFactory.getId(),
         description : $scope.newTask.description
       });
-      $scope.id++;
+      TaskFactory.incId();
     };
     $scope.taskActive = function(task,active){
       $scope.tasks[$scope.tasks.indexOf(task)].active = active;
