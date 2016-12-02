19a62002026826b2ed67480c1f738bd7cf2b8e70
renanslopes8 <renanslopes8@gmail.com>
2016-12-02 11:46:31 -0300

    Updates UI UX and some new features using AngularJS
    Added new features, new UX and UI and some Design Patterns

--- js/controllers/tasks-controller.js
+++ js/controllers/tasks-controller.js
@@ -1,58 +1,74 @@
 /* global taskApp */
 
-  taskApp.factory('TaskFactory', function(){
-    var id =1;
-    var tasks = [
-        {id : id++,description : 'Single Page App with AngularJS',active:'false'},
-        {id : id++,description : 'Learn more',active:'false'},
-        {id : id++,description : 'Just one more task here',active:'false'}
-    ];
-    var factory = {};
-    
-    factory.getTasks = function(){
-      return tasks;
-    };
-    
-    factory.getId = function(){
-      return id;
-    };
-    
-    factory.incId = function(){
-      id ++;
-    };
-  
-    return factory;
-  });
+taskApp.factory('TaskFactory', function(){
+  var id =1;
+  var tasks = [
+  {id : id++,title: 'Front-End Framework AngularJS',description : 'Single Page App with AngularJS',active:'false', progress:'pending'},
+  {id : id++,title: 'Material Design for Bootstrap',description : 'UI (MDB) (Creative Tim)',active:'false',progress:'pending'},
+  {id : id++,title: 'Cordova and PhoneGap',description : 'Application can be run in Smartphone',active:'false',progress:'pending'},
+  {id : id++,title: 'Material Icons',description : 'Material Icons (Google)',active:'false',progress:'pending'},
+  {id : id++,title: 'Task Completed Example',description : 'Just for example task done =)',active:'false',progress:'done'}
+  ];
+  var factory = {};
 
-  taskApp.controller('TaskController', function($scope, TaskFactory,$routeParams){
-    $scope.tasks = [];
-    init();
-    function init(){
-      $scope.tasks = TaskFactory.getTasks();
-    }
-    
-    $scope.addTask = function(){
-      $scope.tasks.push({
-        id : TaskFactory.getId(),
-        description : $scope.newTask.description
-      });
-      TaskFactory.incId();
-    };
-    $scope.taskActive = function(task,active){
-      $scope.tasks[$scope.tasks.indexOf(task)].active = active;
-    };
-    $scope.saveTask = function(task){
-      $scope.tasks[$scope.tasks.indexOf(task)] = {id :  Number(task.id), description: $scope.saveTask.description};
-      $scope.saveTask.description = "";
-      $scope.active = 'false';
-    };
-    $scope.clearTask = function(task){
-      $scope.tasks[$scope.tasks.indexOf(task)] = {id :  Number(task.id), description: ''};
-    };
-    $scope.removeTask = function(task) { 
-      var index = $scope.tasks.indexOf(task);
-      $scope.tasks.splice(index, 1);     
-    };
-  });
+  factory.getTasks = function(){     
+    return tasks;
+  };
+
+  factory.getId = function(){
+    return id;
+  };
+
+  factory.incId = function(){
+    id ++;
+  };
+  
+  return factory;
+});
+
+taskApp.controller('TaskController', function($scope, TaskFactory,$routeParams){
+  $scope.tasks = [];
+  init();
+  function init(){
+    $scope.tasks = TaskFactory.getTasks();
+  }
+
+  $scope.addTask = function(){
+    $scope.tasks.push({
+      id : TaskFactory.getId(),
+      title : $scope.newTask.title,
+      description : $scope.newTask.description,
+      active : 'false',
+      progress: 'pending'
+    });
+    TaskFactory.incId();
+    $scope.newTask.title = '';
+    $scope.newTask.description = '';
+  };
+  $scope.taskActive = function(task,active){
+    $scope.tasks[$scope.tasks.indexOf(task)].active = active;
+    $scope.saveTask.title = $scope.tasks[$scope.tasks.indexOf(task)].title;
+    $scope.saveTask.description = $scope.tasks[$scope.tasks.indexOf(task)].description;
+  };
+  $scope.taskProgress = function(task,progress){
+    $scope.tasks[$scope.tasks.indexOf(task)].progress = progress;
+  };
+  $scope.saveTask = function(task){
+    $scope.tasks[$scope.tasks.indexOf(task)] = {id :  Number(task.id), title: $scope.saveTask.title, description: $scope.saveTask.description,progress: 'pending',active: 'false'};
+    $scope.saveTask.title = "";
+    $scope.saveTask.description = "";
+    $scope.saveTask.description = "";
+  };
+  $scope.taskActive = function(task,active){
+    $scope.tasks[$scope.tasks.indexOf(task)].active = active;
+    $scope.saveTask.title = $scope.tasks[$scope.tasks.indexOf(task)].title;
+    $scope.saveTask.description = $scope.tasks[$scope.tasks.indexOf(task)].description;
+  };
+  $scope.removeTask = function(task) { 
+    var index = $scope.tasks.indexOf(task);
+    $scope.tasks.splice(index, 1);     
+  };
+
+});
 
 
--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ var taskApp = angular.module('taskApp',['ngRoute']);
 
 taskApp.config(['$routeProvider','$locationProvider',
   function($routeProvider, $locationProvider) {
-  $routeProvider.when('/', {
-      templateUrl : 'views/task-list.html',
+  $routeProvider.when('/tasks-list', {
+      templateUrl : 'views/task-progress-list.html',
       controller : 'TaskController'
-   }).when('/dashboard', {
-       templateUrl : 'views/dashboard.html',
+   }).when('/', {
+       templateUrl : 'views/task-progress-list.html',
+       controller : 'TaskController'
+    }).when('/tasks-done', {
+       templateUrl : 'views/task-completed-list.html',
+       controller : 'TaskController'
+    }).when('/about', {
+       templateUrl : 'views/about.html',
        controller : 'TaskController'
     })
    .otherwise ({ redirectTo: '/' });
